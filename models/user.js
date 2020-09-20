431f0684a5e8b29d111dbce9f9891957f979a8a1
Snehal Shinde <42643006+sneshinde@users.noreply.github.com>
2020-09-20 10:36:06 -0700

    added model

---
+++ models/user.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32,
+        unique: true,
+        index: true,
+        lowercase: true
+    }, 
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32,
+        unique: true,
+        lowercase: true
+    },
+    profile: {
+        type: String,
+        required: true,
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    salt: String,
+    about: {
+        type: Number
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    resetPasswordLink: {
+        data: String,
+        default: ''
+    }
+}, {timestamp: true})
+
+userSchema.virtual('password')
+.set(function(password){
+    //create a temporary var called _password
+    this._password = password;
+    //generate salt for hashing algo
+    this.salt=this.makeSalt();
+    //encrypt password
+    this.hashed_password = this.encryptPassword(password);
+})
+.get(function(){
+    return this._password;
+})
+
+userSchema.methods = {
+    authenticate: function(plainPassword) {
+        return this.encryptPassword(plainPassword) === this.hashed_password;
+    },
+    encryptPassword: function (password) {
+        if (!password) return '';
+
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch {
+            return '';
+        }
+    },
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    }
+}
+
+module.exports = mongoose.model('User', userSchema);
