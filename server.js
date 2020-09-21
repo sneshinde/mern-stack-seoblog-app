831555954aa7ebbf6d0fcf4505f0f620d997b0aa
Snehal Shinde <42643006+sneshinde@users.noreply.github.com>
2020-09-20 23:12:44 -0700

    updated static path

--- server.js
+++ server.js
@@ -52,7 +52,9 @@ app.use('/api', formRoutes);
 //Serve static assets if in production
 if(process.env.NODE_ENV == 'production'){
     //set static folder
-    app.use(express.static('frontend/build'));
+    //app.use(express.static('frontend/build'));
+    
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
 
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
